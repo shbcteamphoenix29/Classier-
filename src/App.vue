ce77f5da5e6f3691ed38c0899f16af10039b6f2d
Ayooluwa Adeleke <51849584+PolyMath-Coder@users.noreply.github.com>
2022-01-07 17:12:43 +0100

    Initial Commit and Project SetUp

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageIndex />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageIndex from '@/components/PageIndex.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    PageIndex,
+  },
+};
 </script>
 
 <style>
